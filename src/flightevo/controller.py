4d0adfcdc843940646eb947126efa1501aeafb47
Yvo Keuter <ypmkeuter@gmail.com>
2022-03-23 09:45:13 +0100

    add utils

--- src/flightevo/controller.py
+++ src/flightevo/controller.py
@@ -1,7 +1,6 @@
 import rospy
 from flightros.msg import Cmd, State
 from sensor_msgs.msg import Image
-import math
 
 
 class Controller:
@@ -54,19 +53,3 @@ class Controller:
 
     def reset_callback(self, msg):
         pass
-
-    def quaternion_to_euler(self, x, y, z, w):
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll = math.atan2(t0, t1)
-
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch = math.asin(t2)
-
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw = math.atan2(t3, t4)
-
-        return roll, pitch, yaw
