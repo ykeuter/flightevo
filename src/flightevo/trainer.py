391d21949c45abcd42510783149b1c58988c163b
Yvo Keuter <ypmkeuter@gmail.com>
2022-03-23 09:45:28 +0100

    calc reward

--- src/flightevo/trainer.py
+++ src/flightevo/trainer.py
@@ -1,14 +1,23 @@
 import rospy
+import os
+import neat
+import numpy as np
 from flightros.msg import State
 from flightros.srv import ResetSim, ResetSimRequest, ResetCtl, ResetCtlRequest
 from geometry_msgs.msg import Pose, Point, Quaternion, Vector3, Twist
-import os
-import neat
+
+from . import utils
 from .mlp import Mlp
 
 
 class Trainer:
     EPOCH_LENGTH = 5.0
+    POS_COEFF = -0.002
+    ORI_COEFF = -0.002
+    LIN_VEL_COEFF = -0.0002
+    ANG_VEL_COEFF = -0.0002
+    ACT_COEFF = -0.0002
+    GOAL_STATE = np.array([.0, .0, 5., .0, .0, .0, .0, .0, .0, .0, .0, .0])
 
     def __init__(self):
         self._reset_sim = None
@@ -18,7 +27,8 @@ class Trainer:
         self._generator = None
         self._current_reward = 0
         self._current_agent = None
-        self._prev_t = 0
+        self._prev_state_t = 0
+        self._prev_cmd_t = 0
 
     def run(self):
         config_path = os.path.join(os.path.dirname(__file__), "nsssssssat.cfg")
@@ -68,3 +78,28 @@ class Trainer:
         if t > self.EPOCH_LENGTH:
             self._is_resetting = True
             self._reset()
+
+    def _get_reward(self, s):
+        r = 0
+
+        v = np.array([s.pose.position.x, s.pose.position.y, s.pose.position.z])
+        d = v - self.GOAL_STATE[:3]
+        r += np.inner(d, d) * self.POS_COEFF
+
+        v = np.array(utils.quaternion_to_euler(
+            s.pose.orientation.x, s.pose.orientation.y, s.pose.orientation.z,
+            s.pose.orientation.w
+        ))
+        d = v - self.GOAL_STATE[3:6]
+        r += np.inner(d, d) * self.ORI_COEFF
+
+        v = np.array(
+            [s.twist.linear.x, s.potwistse.linear.y, s.twist.linear.z])
+        d = v - self.GOAL_STATE[6:9]
+        r += np.inner(d, d) * self.LIN_VEL_COEFF
+
+        v = np.array([s.twist.angular.x, s.twist.angular.y, s.twist.angular.z])
+        d = v - self.GOAL_STATE[9:]
+        r += np.inner(d, d) * self.ANG_VEL_COEFF
+
+        return r
