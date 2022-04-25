374f057d28f52ec97faae02c54fa5c22f1476166
Yvo Keuter <ypmkeuter@gmail.com>
2022-04-25 10:19:12 +0200

    no hidden layers

--- src/flightevo/dodger.py
+++ src/flightevo/dodger.py
@@ -36,40 +36,41 @@ class Dodger:
         r = 5
 
         inputs = []
-        z = 0
+        # z = 0
         grid = self._get_grid(
             self._resolution_width, self._resolution_height, r * 2, r * 2)
-        img = [(x, y, z) for x, y in grid]
-        inputs += img
+        # img = [(x, y, z) for x, y in grid]
+        # inputs += img
 
-        hidden1 = []
-        z = 1
-        grid = self._get_grid(4, 4, r * 2, r * 2)  # max 12x12
-        layer1 = [(x, y, z) for x, y in grid]
-        hidden1 += layer1
-        z = 2
-        layer2 = [(x, y, z) for x, y in grid]
-        hidden1 += layer2
+        # hidden1 = []
+        # z = 1
+        # grid = self._get_grid(4, 4, r * 2, r * 2)  # max 12x12
+        # layer1 = [(x, y, z) for x, y in grid]
+        # hidden1 += layer1
+        # z = 2
+        # layer2 = [(x, y, z) for x, y in grid]
+        # hidden1 += layer2
 
-        hidden2 = []
-        z = 3
-        grid = self._get_grid(4, 4, r * 2, r * 2)  # max 8x8
-        layer1 = [(x, y, z) for x, y in grid]
-        hidden2 += layer1
-        z = 4
-        layer2 = [(x, y, z) for x, y in grid]
-        hidden2 += layer2
+        # hidden2 = []
+        # z = 3
+        # grid = self._get_grid(4, 4, r * 2, r * 2)  # max 8x8
+        # layer1 = [(x, y, z) for x, y in grid]
+        # hidden2 += layer1
+        # z = 4
+        # layer2 = [(x, y, z) for x, y in grid]
+        # hidden2 += layer2
 
-        z = 5
+        # z = 5
         outputs = [
-            (0, r, z),  # up
-            (0, -r, z),  # down
-            (r, 0, z),  # right
-            (-r, 0, z),  # left
-            (0, 0, z),  # forward
+            (0, r, ),  # up
+            (0, -r, ),  # down
+            (r, 0, ),  # right
+            (-r, 0, ),  # left
+            (0, 0, ),  # forward
         ]
 
-        return [inputs, hidden1, hidden2, outputs]
+        # return [inputs, hidden1, hidden2, outputs]
+        return [grid, outputs]
 
     def _get_grid(self, ncols, nrows, width, height):
         return [
@@ -86,7 +87,7 @@ class Dodger:
         k0 = int(r / self._resolution_height)
         k1 = int(c / self._resolution_width)
         # copy needed due to non-writeable nparray
-        new_img = 1 - torch.tensor(img) \
+        new_img = torch.tensor(img) \
             .unfold(0, k0, k0).unfold(1, k1, k1).amin((-1, -2),)
         # print(r, c)
         # cv2.imshow("depth resized", new_img.numpy())
