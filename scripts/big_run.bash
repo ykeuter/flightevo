122c22d894db0aff998ae58c08cca5a0ef749935
Yvo Keuter <ypmkeuter@gmail.com>
2022-06-16 01:10:32 +0200

    adding more script

--- scripts/big_run.bash
+++ scripts/big_run.bash
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 DIR="logs/bigrun"
-env="$DIR/env-training.yaml"
-neat="$DIR/neat.cfg"
+
 for i in {0..19}
 do
+  # training
+  env="$DIR/env-training.yaml"
+  neat="$DIR/neat.cfg"
   log="$DIR/run-$i"
-  python -m fligtevo.dodge_trainer --neat $neat --env $env --log $log
+  python -m flightevo.dodge_trainer --neat $neat --env $env --log $log
+  # evaluatin for selection
+  out="$log/selection-stats.csv"
+  env="$DIR/env-selection.yaml"
+  checkpoint="$(ls $log/environment_*.pickle | sort -Vr | head -1)"
+  python -m flightevo.evaluator --out $out --env $env --checkpoint $checkpoint
 done
