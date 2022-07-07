5b1d9c2045db4be4412ae472daa3a6ace3335247
Yvo Keuter <ypmkeuter@gmail.com>
2022-07-07 13:16:19 +0200

    genome analysis

--- sandboxes/sandbox.py
+++ sandboxes/sandbox.py
@@ -1,9 +1,22 @@
 import pickle
+from pathlib import Path
 
 
-fn_in = "logs/bigrun-3/run-4/run-4-checkpoint-19-6.pickle"
+DIR = Path("logs/bigrun-7")
 
-with open(fn_in, "rb") as f:
-    g = pickle.load(f)
-
-print("finish")
+for p in DIR.rglob("run-*.pickle"):
+    print(p)
+    with open(p, "rb") as fi:
+        g = pickle.load(fi)
+    nodes = list(g.nodes.values())
+    nodes.sort(key=lambda x: -abs(x.weight))
+    tot = sum(abs(n.weight) for n in nodes)
+    for n in nodes:
+        with open(DIR / "genomes.csv", "a") as fo:
+            fo.write("{},{},{},{},{}\n".format(
+                p.stem,
+                abs(n.weight) / tot,
+                n.weight,
+                n.zoom,
+                n.scale,
+            ))
