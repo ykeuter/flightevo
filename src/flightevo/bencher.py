66223afe69859a45416a9bf8ce8522be4c23a8a6
Yvo Keuter <ypmkeuter@gmail.com>
2022-10-15 20:26:13 +0200

    adding yaw and z adjustment calc

--- src/flightevo/bencher.py
+++ src/flightevo/bencher.py
@@ -1,5 +1,7 @@
 from flightevo.dodger import Dodger
 from flightevo import utils
+import numpy as np
+import quaternion
 
 
 class Bencher(Dodger):
@@ -8,25 +10,42 @@ class Bencher(Dodger):
                  creep_z, creep_yaw):
         self._creep_z = creep_z
         self._creep_yaw = creep_yaw
-        self._target_x, self._target_y, self._target_z = 0, 0, 0
+        self._target = np.array([0., 0., 0.], dtype=np.float32)
         super().__init__(resolution_width, resolution_height,
                          speed_x, speed_y, speed_z, bounds, gamma, acc, margin)
 
     def set_target(self, x, y, z):
-        self._target_x, self._target_y, self._target_z = x, y, z
+        self._target = np.array([x, y, z], dtype=np.float32)
 
     def compute_command_vision_based(self, state, img):
         # s = self._transform_state(state)
         i = self._transform_img(img, state)
         a = self._mlp.activate(i)
         v = self._transform_activations(a, state)
-        v = self._adjust_height(v, state)
+        v = self._adjust_z(v, state)
         yawrate = self._adjust_yaw(state)
         c = utils.AgileCommand(2)
         c.t = state.t
         c.velocity = v
         c.yawrate = yawrate
         return c
+
+    def _adjust_z(self, v, state):
+        if state.pos[2] > self._target[2]:
+            v[2] -= self._creep_z
+        else:
+            v[2] += self._creep_z
+        return v
+
+    def _adjust_yaw(self, state):
+        t = self._target - state.pos
+        rq = np.quaternion(*state.att)
+        yq = np.quaternion(0, 0, 1, 0)
+        y = rq * yq * np.conjugate(rq)
+        a = np.dot(t, y.imag)
+        if a > 0:
+            return self._creep_yaw
+        return -self._creep_yaw
 
     def _transform_activations(self, a, state):
         # a: up, right, down, left, center
@@ -50,5 +69,8 @@ class Bencher(Dodger):
             vz = -self._speed_z
         elif index == 3:  # left
             vy = self._speed_y
-        vx, vy, vz = utils.translate(vx, vy, vz, *state.att)
-        return [vx, vy, vz]
+
+        vq = np.quaternion(0, vx, vy, vz)
+        rq = np.quaternion(*state.att)
+        v = np.conjugate(rq) * vq * rq
+        return [*v.imag]
