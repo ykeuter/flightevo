374c8fd1a06ecde4cec5c08b3f4882c3481ff9b3
Yvo Keuter <ypmkeuter@gmail.com>
2022-11-11 18:44:13 +0100

    rename

--- src/flightevo/bench_trainer.py
+++ src/flightevo/bench_trainer.py
@@ -10,13 +10,13 @@ import pickle
 import roslaunch
 from threading import Lock
 from pathlib import Path
-from dodgeros_msgs.msg import QuadState, Command
-from envsim_msgs.msg import ObstacleArray
 from sensor_msgs.msg import Image
-from std_msgs.msg import Empty, Bool
+from std_msgs.msg import Bool
 from cv_bridge import CvBridge
+from nav_msgs.msg import Odometry, Path as PathMsg
 from ruamel.yaml import YAML
 from geometry_msgs.msg import TwistStamped
+from avoid_msgs.msg import TaskState
 from neat.csv_reporter import CsvReporter
 from neat.winner_reporter import WinnerReporter
 from neat.function_reporter import FunctionReporter
@@ -81,9 +81,7 @@ class BenchTrainer:
             creep_z=config["dodger"]["creep_z"],
             creep_yaw=config["dodger"]["creep_yaw"],
         )
-        self._target = np.array(config['environment']['target'],
-                                dtype=np.float32)
-        self._dodger.set_target(self._target)
+        self._target = None
         self._timeout = config['environment']['timeout']
         self._bounding_box = np.reshape(np.array(
             config['environment']['world_box'], dtype=np.float32), (3, 2))
@@ -105,115 +103,49 @@ class BenchTrainer:
                 for i in rng.sample(range(r[0], r[1]), r[1] - r[0])
             )
         self._current_level = None
-
+    
     def run(self):
         rospy.Subscriber(
-            "/kingfisher/dodgeros_pilot/state", QuadState, self.state_callback,
+            "/hummingbird/ground_truth/odometry",
+            Odometry, self.state_callback,
             queue_size=1, tcp_nodelay=True)
         rospy.Subscriber(
-            "/kingfisher/dodgeros_pilot/unity/depth", Image, self.img_callback,
+            "/depth", Image, self.img_callback,
             queue_size=1, tcp_nodelay=True)
         rospy.Subscriber(
-            "/kingfisher/dodgeros_pilot/groundtruth/obstacles", ObstacleArray,
+            "/hummingbird/collision", Bool,
             self.obstacle_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber(
+            "/hummingbird/goal_point", PathMsg, self.target_callback,
+            queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber(
+            "/hummingbird/task_state", TaskState, self.task_callback,
+            queue_size=1, tcp_nodelay=True)
+
         self._cmd_pub = rospy.Publisher(
-            "/kingfisher/dodgeros_pilot/velocity_command", TwistStamped,
+            "/hummingbird/autopilot/velocity_command", TwistStamped,
             queue_size=1)
-        self._off_pub = rospy.Publisher(
-            "/kingfisher/dodgeros_pilot/off", Empty, queue_size=1)
-        self._reset_pub = rospy.Publisher(
-            "/kingfisher/dodgeros_pilot/reset_sim", Empty, queue_size=1)
-        self._enable_pub = rospy.Publisher(
-            "/kingfisher/dodgeros_pilot/enable", Bool, queue_size=1)
-        self._start_pub = rospy.Publisher(
-            "/kingfisher/dodgeros_pilot/start", Empty, queue_size=1)
-        self._rluuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        roslaunch.configure_logging(self._rluuid)
-        self._launch()
-        self._reset()
         rospy.spin()
-
-    def _level_up(self):
-        self._roslaunch.shutdown()
-        self._launch()
-
-    def _launch(self):
-        fn = "/home/ykeuter/flightevo/cfg/simulator.launch"
-        try:
-            self._current_level = next(self._levels)
-            # self._current_level = random.choice(self._levels)
-        except StopIteration:
-            rospy.signal_shutdown("No more environments!")
-            raise
-        args = [
-            "env:={}".format(self._current_level),
-            "cfg:={}".format(Path(self._env_cfg).resolve()),
-        ]
-        self._roslaunch = roslaunch.parent.ROSLaunchParent(
-            self._rluuid, [(fn, args)])
-        self._roslaunch.start()
-        time.sleep(10.)
-
-    def _reset(self):
-        self._active = False
-        Thread(target=self._reset_and_wait).start()
-
-    def _reset_and_wait(self):
-        try:
-            self._current_genome = next(self._generator)
-        except neat.CompleteExtinctionException:
-            rospy.signal_shutdown("No more genomes!")
-            raise
-        except StopIteration:
-            rospy.signal_shutdown("Found solution!")
-            raise
-            # self._winner_reporter.reset(self._current_level)
-            # self._generator = iter(self._population)
-            # self._current_genome = next(self._generator)
-            # self._level_up()
-        self._current_genome.fitness = 0
-        with self._lock:
-            self._dodger.load(self._current_genome)
-        self._start_time = None
-        self._state = None
-        # make sure dodger has no more cached actions
-        time.sleep(.1)
-        self._off_pub.publish()
-        # make sure off signal is processed
-        time.sleep(.1)
-        self._reset_pub.publish()
-        self._enable_pub.publish(True)
-        # make sure reset is processed
-        time.sleep(.1)
-        self._start_pub.publish()
-        # make sure drone took off
-        time.sleep(.5)
-        self._crashed = False
-        self._active = True
 
     def state_callback(self, msg):
         if not self._active:
             return
         if self._crashed:
             print("crashed")
-            return self._reset()
         if self._start_time is None:
-            self._start_time = msg.t
+            self._start_time = msg.header.stamp
         if msg.t - self._start_time > self._timeout:
             print("timeout")
-            return self._reset()
-        pos = np.array([msg.pose.position.x,
-                        msg.pose.position.y,
-                        msg.pose.position.z])
-        vel = np.array([msg.velocity.linear.x,
-                        msg.velocity.linear.y,
-                        msg.velocity.linear.z])
+        #     return self._reset()
+        pos = np.array([msg.pose.pose.position.x,
+                        msg.pose.pose.position.y,
+                        msg.pose.pose.position.z])
         if (
             (pos <= self._bounding_box[:, 0]) |
             (pos >= self._bounding_box[:, 1])
         ).any():
             print("oob")
-            return self._reset()
+        #     return self._reset()
         d = np.linalg.norm(pos - self._target)
         # self._current_genome.fitness = msg.pose.position.x
         self._current_genome.fitness = max(self._current_genome.fitness,
@@ -221,15 +153,17 @@ class BenchTrainer:
         # if msg.pose.position.x >= self._xmax:
         if d <= 1.:
             print("success")
-            return self._reset()
+            # return self._reset()
         self._state = AgileQuadState(msg)
 
     def img_callback(self, msg):
         if not self._active:
             return
-        # store state in case of reset
         s = self._state
         if s is None:
+            return
+        t = self._target
+        if t is None:
             return
         cv_image = self._cv_bridge.imgmsg_to_cv2(
             msg, desired_encoding='passthrough')
@@ -248,11 +182,20 @@ class BenchTrainer:
     def obstacle_callback(self, msg):
         if not self._active:
             return
-        o = msg.obstacles[0]
-        d = np.linalg.norm(np.array(
-            [o.position.x, o.position.y, o.position.z]))
-        if d - o.scale < 0:
-            self._crashed = True
+        self._crashed = msg.data
+
+    def task_callback(self, msg):
+        if (msg.Mission_state is not TaskState.PREPARING and
+            msg.Mission_state is not TaskState.UNITYSETTING and
+            msg.Mission_state is not TaskState.GAZEBOSETTING
+        ):
+            self._active = True
+
+    def target_callback(self, msg):
+        self._target[0] = msg.poses[0].pose.position.x
+        self._target[1] = msg.poses[0].pose.position.y
+        self._target[2] = msg.poses[0].pose.position.z
+        self._dodger.set_target(self._target)
 
 
 if __name__ == "__main__":
